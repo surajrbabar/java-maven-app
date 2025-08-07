c236c12873158de810a3817b8e4f3c2239eec99e
Suraj Babar <surajrbabar@gmail.com>
2025-08-07 15:32:50 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
 def buildApp(){
     echo "building the jar file..."
-    sh "mvn package"
+    sh "mvn clean package"
 }
 
 def buildImage(){
     echo "building the docker image ..."
     withCredentials([usernamePassword(credentialsId : 'dockerhub-credentials', usernameVariable : 'USER', passwordVariable : 'PASS')]){
-        sh 'docker build -t surajrbabar/java-maven-app:jma-1.2 .'
+        sh "docker build -t surajrbabar/java-maven-app:${IMAGE_NAME} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push surajrbabar/java-maven-app:jma-1.2'
+        sh "docker push surajrbabar/java-maven-app:${IMAGE_NAME}"
     }
 }
 
