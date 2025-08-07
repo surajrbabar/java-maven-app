a2cfc56a277f757e6a302d882fa2422a8b66b75b
Suraj Babar <surajrbabar@gmail.com>
2025-08-07 17:35:37 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -23,7 +23,7 @@ def commit(){
         sh 'git branch'
         sh 'git config --list'
 
-        sh "git remote set-url origin https://${USER}:${PASS}@github.com/surajrbabar/java-maven-app.git"
+        sh "git remote set-url origin https://${USER}:${safePass}@github.com/surajrbabar/java-maven-app.git"
         sh 'git add .'
         sh 'git commit -m "ci : version bump"'
         sh 'git push origin HEAD:jenkins-jobs'
