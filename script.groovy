c2c8eb01cfbef7377a31a7f020edd65f0333178a
Suraj Babar <surajrbabar@gmail.com>
2025-08-14 12:51:20 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage(IMAGE) {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t ${IMAGE} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push ${IMAGE}"
